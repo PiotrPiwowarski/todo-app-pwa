0baba1027ed9ddc6b30e3f08b4b6ce85ad2416f9
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-10-26 14:34:09 +0200

    new commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,26 @@
+import {useState} from 'react';
+
 const Categories = () => {
+
+	const [button, setButton] = useState('new');
+
+	const handleClickNew = () => {
+		setButton('new');
+	}
+
+	const handleClickActive = () => {
+		setButton('active');
+	}
+
+	const handleClickFinished = () => {
+		setButton('finished');
+	}
+
 	return (
 		<div className='aside-menu'>
-			<button>NOWE</button>
-			<button>W TRAKCIE</button>
-			<button>ZAKOŃCZONE</button>
+			<button onClick={handleClickNew} style={button === 'new' ? {textDecoration: 'underline'} : {}}>NOWE</button>
+			<button onClick={handleClickActive} style={button === 'active' ? {textDecoration: 'underline'} : {}}>AKTYWNE</button>
+			<button onClick={handleClickFinished} style={button === 'finished' ? {textDecoration: 'underline'} : {}}>ZAKOŃCZONE</button>
 		</div>
 	);
 };
